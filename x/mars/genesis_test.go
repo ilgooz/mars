ba183fb25e3081f175ae47b9f4d0c5f55affd766
Developer Experience team at Tendermint <hello@tendermint.com>
2022-05-18 18:00:18 +0300

    Initialized with Ignite CLI

---
+++ x/mars/genesis_test.go
@@ -0,0 +1,29 @@
+package mars_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	keepertest "mars/testutil/keeper"
+	"mars/testutil/nullify"
+	"mars/x/mars"
+	"mars/x/mars/types"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.MarsKeeper(t)
+	mars.InitGenesis(ctx, *k, genesisState)
+	got := mars.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
